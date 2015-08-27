99de08ed54d2bde8ecdd1321c84a07aaf8e89ec4
Till Reiter <till_signavio@Tills-MacBook-Pro-2.local>
2015-08-27 18:13:01 +0200

    set up first version of task manager with mock data

---
+++ iOS/iosclientTests/iosclientTests.m
@@ -0,0 +1,9 @@
+//
+//  iosclientTests.m
+//  iosclient
+//
+//  Created by Till Reiter on 27/08/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,116 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+ * Effektif iOS Client
+ * https://github.com/effektif/ios-client
+*/
 
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
-  View,
+  View
 } = React;
 
+var MOCKED_TASK_DATA = [
+  {
+    assigneeId: '55ded6d3e4b056adca2f3a83',
+    candidateIds: [],
+    caze: {id: '55ded6b0e4b056adca2f3a62', name: 'Code Camp 2015'},
+    cazeId: '55ded6b0e4b056adca2f3a62',
+    createdBy: '537f6807e4b011fbfef4e614',
+    id: '55ded6c4e4b056adca2f3a7b',
+    lastUpdated: '2015-08-27T09:22:12.253Z',
+    name: 'Call Micheal',
+    organizationId: '537f6807e4b011fbfef4e60b',
+    parentId: '55ded6b0e4b056adca2f3a62',
+    participantIds: ['537f6807e4b011fbfef4e614', '55ded6d3e4b056adca2f3a83']
+  },
+  {
+    assigneeId: '55ded6d3e4b056adca2f3a83',
+    candidateIds: [],
+    caze: {id: '55ded6b0e4b056adca2f3a62', name: 'Code Camp 2015'},
+    cazeId: '55ded6b0e4b056adca2f3a62',
+    createdBy: '537f6807e4b011fbfef4e614',
+    id: '55ded6c4e4b056adca2f3a7b',
+    lastUpdated: '2015-08-27T09:22:12.253Z',
+    name: 'Check Finance',
+    organizationId: '537f6807e4b011fbfef4e60b',
+    parentId: '55ded6b0e4b056adca2f3a62',
+    participantIds: ['537f6807e4b011fbfef4e614', '55ded6d3e4b056adca2f3a83']
+  }
+];
+
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
 var iosclient = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      }),
+      loaded: false
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(MOCKED_TASK_DATA),
+          loaded: true
+        });
+      })
+      .done();
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderTask}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text>
+          Loading Effektif...
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      </View>
+    );
+  },
+
+  renderTask: function(task) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.checkBoxContainer}>
+          <Text style={styles.avatarInitials}>TR</Text>
+        </View>
+        <View style={styles.avatarContainer}>
+          <Text style={styles.avatarInitials}>TR</Text>
+        </View>
+        <View style={styles.taskNameContainer}>
+          <Text style={styles.title}>{task.name}</Text>
+          <Text style={styles.subtitle}>{task.caze.name}</Text>
+        </View>
       </View>
     );
   }
@@ -34,20 +119,51 @@ var iosclient = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: 42,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#f2f2f2',
+    marginHorizontal: 10,
+    marginBottom: 10
   },
-  welcome: {
-    fontSize: 20,
+  checkBoxContainer: {
+    backgroundColor: '#9cc8ca',
+    height: 42,
+    width: 42
+  },
+  avatarContainer: {
+    backgroundColor: '#d960b4',
+    height: 42,
+    width: 42
+  },
+  avatarInitials: {
+    fontSize: 24,
+    color: 'white',
     textAlign: 'center',
-    margin: 10,
+    'justifyContent': 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  taskNameContainer: {
+    flex: 1,
+    marginHorizontal: 10
   },
+  title: {
+    fontSize: 16,
+    overflow: 'hidden',
+    color: '#128187'
+  },
+  subtitle: {
+    fontSize: 12,
+    color: '#989898'
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: 'white'
+  }
 });
 
 AppRegistry.registerComponent('iosclient', () => iosclient);
