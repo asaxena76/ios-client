8b69e6bdf1b87e0ac9b9ad198b1c8c7d88fa3080
MaVo <markus.vogt@thinktable.de>
2015-08-29 10:23:54 +0200

    tabbar test

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -29,7 +29,8 @@ module.exports = React.createClass({
   getInitialState: function() {
     return {
       dataSource: null,
-      loaded: false
+      loaded: false,
+      selectedTab: 'all'
     };
   },
 
@@ -49,7 +50,21 @@ module.exports = React.createClass({
         });
       });
 
-    // return MOCKED_TASK_DATA;
+  },
+
+  fetchInboxData: function() {
+  	// we should change the data source when switching tabs
+    request
+      .get("https://ci.effektif.com/api/v1/effektif/tasks?involvement%5B%5D=assignedToMe&involvement%5B%5D=imaCandidate&involvement%5B%5D=iStarted&completed=false")
+      .set("Authorization", this.props.token)
+      .end((error, response) => {
+        console.log(error, response);
+        this.setState({
+          dataSource: ds.cloneWithRows(response.body),
+          loaded: true
+        });
+      });
+
   },
 
   render: function() {
@@ -58,12 +73,61 @@ module.exports = React.createClass({
     }
 
     return (
-      <ListView
+    	// insert tabbar here
+
+    	<TabBarIOS
+	        tintColor="white"
+	        barTintColor="darkslateblue">
+
+	        <TabBarIOS.Item
+	          title="Inbox"
+	          icon={{uri: base64Icon, scale: 3}}
+	          selected={this.state.selectedTab === 'inbox'}
+	          onPress={() => {
+	            this.setState({
+	              selectedTab: 'inbox',
+	            });
+	          }}>
+			  {this.renderInbox()}
+        	</TabBarIOS.Item>
+
+			<TabBarIOS.Item
+	          title="All"
+	          icon={{uri: base64Icon, scale: 3}}
+	          selected={this.state.selectedTab === 'all'}
+	          onPress={() => {
+	            this.setState({
+	              selectedTab: 'all',
+	            });
+	          }}>
+			  {this.renderInbox()}
+        	</TabBarIOS.Item>
+        </TabBarIOS>
+    	// end of tabbar stuff
+      
+    );
+  },
+
+  renderInbox: function() {
+  	// loaded set to false before?
+  	this.fetchInboxData()
+
+  	return <ListView
         dataSource={this.state.dataSource}
         renderRow={this.renderTask}
         style={styles.listView}
       />
-    );
+  },
+
+  renderAll: function() {
+  	// loaded set to false before?
+  	this.fetchData()
+
+  	return <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderTask}
+        style={styles.listView}
+      />
   },
 
   renderLoadingView: function() {
